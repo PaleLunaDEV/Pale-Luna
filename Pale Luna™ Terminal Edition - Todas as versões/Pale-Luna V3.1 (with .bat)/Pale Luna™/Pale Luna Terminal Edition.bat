c86e1c3524388fae63ad6d71714fba6ba604013a
Moonchildfi <lucaseduarte6@gmail.com>
2025-11-12 15:59:01 -0300

    PL-3.1

---
+++ Pale Luna™ Terminal Edition - Todas as versões/Pale-Luna V3.1 (with .bat)/Pale Luna™/Pale Luna Terminal Edition.bat
@@ -0,0 +1,51 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGqh2mMZFAtVQAHPMH60B4ks6eT+r8aSrEyfsCB/TIrJ172AIexTz0HtYZ8i2DoX2P8DCBpLPhO4Lhkxu33hY2GWC/edvwqsZl2H5wsGSCslxyvHhT8zIJ1qm9dK7xC/8Ei/s7Ae0PobnJUPBXPeyK9nNocJ/gXU
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGqh2mMZFAtVQAHPMH60B4ks6eT+r8aSrEyfsCB/TIrJ172AIexTz0HtYZ8i2DoX2P8DCBpLPhO4Lhkxu33hY2GWC/edvwqsZl2H5wsGSCslxyvHhT8zIJ1qm9dK7xC/8Ei/s7Ae0PobnJU+FG/kgYB9NshO+BKxCA==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title Pale Luna
+color 0B
+where node >nul
+if errorlevel 1 (
+  echo Node.js nao encontrado. Instale para continuar.
+  pause
+  exit
+)
+cd assets
+node lang.js
+cd..
+if not exist HAHAHAHAHAHAHA.txt (
+taskkill /f /im "vlc.exe" 2>NUL
+)
+
+pause
+exit /b 0
